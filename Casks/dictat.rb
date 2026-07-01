8f6ffb4077303f6be0c41521014e8862d638534d
Marc Franquesa Monés <git@marcfranquesa.com>
2026-07-01 10:15:36 +0200

    Pin initial cask checksum

--- Casks/dictat.rb
+++ Casks/dictat.rb
@@ -1,6 +1,6 @@
 cask "dictat" do
   version "0.1.0"
-  sha256 :no_check
+  sha256 "adf14c9f35c481e2f2b3033082cbb93eb67babeaca0772062dea2f0a15e06991"
 
   url "https://github.com/marcfranquesa/dictat/releases/download/v#{version}/Dictat.zip",
       verified: "github.com/marcfranquesa/dictat/"
